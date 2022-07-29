6b4bcc3e83f2c10e21bbf91bd49065ab6e2b1e11
olasrum <mursalo@gmail.com>
2022-07-29 20:30:29 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+let people = ['Sven', 'Glen', 'Susy', 'Gladys']
+
+function findMatching(drivers, string) {
+    return drivers.filter(function(s) {
+        return s.toLowerCase().includes(string.toLowerCase());
+    })
+}
+
+console.log(findMatching(people, 'en'))
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(s) { 
+        return s.startsWith(string);
+    })
+}
+
+console.log(findMatching(people, 'S'))
+
+function matchName(drivers, string) {
+    return drivers.filter(function(s) {
+        return s.name === string;
+    })
+}
